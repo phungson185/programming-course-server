5c4cf44df8fd208cb937c29a55d278a230481f55
phungson185 <phungson185@gmail.com>
2022-07-11 22:09:00 +0700

    fix: not response when middleware error

--- middlewares/authAdmin.js
+++ middlewares/authAdmin.js
@@ -6,27 +6,30 @@ const ApiError = require('../utils/ApiError');
 
 const authAdmin = async (req, res, next) => {
   let token;
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
     token = req.headers.authorization.split(' ')[1];
   }
 
   if (!req.headers.authorization) {
-    throw new ApiError(httpStatus.UNAUTHORIZED, 'No token provided');
+    next(new ApiError(httpStatus.UNAUTHORIZED, 'No token provided'));
   }
 
   const decoded = jwt.verify(token, config.jwt.secret);
   if (decoded) {
     let user = await userService.getUserById(decoded.sub);
     if (!user) {
-      throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
+      next(new ApiError(httpStatus.UNAUTHORIZED, 'Invalid token'));
     }
 
     if (decoded.exp < Date.now() / 1000) {
-      throw new ApiError(httpStatus.UNAUTHORIZED, 'Token expired');
+      next(new ApiError(httpStatus.UNAUTHORIZED, 'Token expired'));
     }
 
     if (user.role !== 'admin') {
-        throw new ApiError(httpStatus.UNAUTHORIZED, 'User is not admin');
+      next(new ApiError(httpStatus.UNAUTHORIZED, 'You are not admin'));
     }
 
     req.user = user;
