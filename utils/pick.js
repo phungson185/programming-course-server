2a3add532a1fa4dfb05799112a35433702806a56
phungson185 <phungson185@gmail.com>
2022-07-11 13:32:41 +0700

    feat: add list course

--- utils/pick.js
+++ utils/pick.js
@@ -1,3 +1,9 @@
+/**
+ * Create an object composed of the picked object properties
+ * @param {Object} object
+ * @param {string[]} keys
+ * @returns {Object}
+ */
 const pick = (object, keys) => {
   return keys.reduce((obj, key) => {
     if (object && Object.prototype.hasOwnProperty.call(object, key)) {
