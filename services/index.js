967fafdf6bdeffde2a90c6768a20837dbc4b4302
phungson185 <phungson185@gmail.com>
2022-07-11 11:01:11 +0700

    feat: refactor init, readme and fix bug attend

--- services/index.js
+++ services/index.js
@@ -4,3 +4,4 @@ module.exports.userService = require('./user.service');
 module.exports.categoryService = require('./category.service');
 module.exports.courseService = require('./course.service');
 module.exports.attendanceService = require('./attendance.service');
+module.exports.tokenService = require('./token.service');
